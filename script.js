d8c6c1280e487b0427a2e1b040e92ee66b5c4fee
Діана Пахалюк <110489496+Diana323@users.noreply.github.com>
2025-01-05 20:55:27 +0200

    /////

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('burger-icon').addEventListener('click', function() {
+ const menuList = document.getElementById('menu-list');
+ menuList.classList.toggle('burger-active'); // Переключаем класс для отображения меню
+});
